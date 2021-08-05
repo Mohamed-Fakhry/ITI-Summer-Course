9a829ccb38fd2de045b386403a618b5663ad174a
Mohamed Fakhry <mohamed.fakhry@hamza.solutions>
2021-08-05 12:50:51 +0200

    Named navigation

--- lib/main.dart
+++ lib/main.dart
@@ -13,76 +13,261 @@ class App extends StatelessWidget {
         primaryColor: Colors.blue,
         accentColor: Colors.red,
       ),
-      home: ITIScreen(),
+      // home: MoviesScreen(),
+      routes: {
+        "/": (_) => MoviesScreen(),
+        "/movie-details": (_) => MovieDetails(),
+      },
     );
   }
 }
 
-class ITIScreen extends StatelessWidget {
+class MoviesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff333333),
       appBar: AppBar(
-        title: Text("ITI Screen"),
+        title: Text("Movies Screen"),
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          margin: EdgeInsets.all(30),
-          child: Center(
-            child: Column(
-              children: [
-                Image.network(
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Google-flutter-logo.svg/2560px-Google-flutter-logo.svg.png"),
-                Image.network(
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Google-flutter-logo.svg/2560px-Google-flutter-logo.svg.png"),
-                Image.network(
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Google-flutter-logo.svg/2560px-Google-flutter-logo.svg.png"),
-                Container(
-                  height: 120,
-                  width: 120,
-                  color: Colors.red,
-                  child: Text(
-                    "0",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w100,
+      body: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            GestureDetector(
+              onTap: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context) => MovieDetails("Star wars IV"),
+                //   ),
+                // );
+                Navigator.pushNamed(
+                  context,
+                  "/movie-details",
+                  arguments: "Star wars IV",
+                );
+              },
+              child: Container(
+                height: 120,
+                margin: EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Image.network(
+                          "https://m.media-amazon.com/images/I/A1wnJQFI82L._AC_SY879_.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
-                  ),
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Star wars IV",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 19,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Row(
+                              children: [
+                                Text(
+                                  "5 Starts",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Spacer(),
+                                Text(
+                                  "2.6k reviews",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                              ],
+                            ),
+                            SizedBox(height: 5),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.access_time,
+                                  size: 14,
+                                  color: Colors.grey[350],
+                                ),
+                                SizedBox(width: 4),
+                                Text(
+                                  "2h 14m",
+                                  style: TextStyle(
+                                    color: Colors.grey[350],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 5),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.access_time,
+                                  size: 14,
+                                  color: Colors.grey[350],
+                                ),
+                                SizedBox(width: 4),
+                                Text(
+                                  "2019/12/31",
+                                  style: TextStyle(
+                                    color: Colors.grey[350],
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-                Icon(Icons.access_time_sharp),
-                Container(
-                  height: 110,
-                  width: 110,
-                  color: Colors.blue,
-                  child: Text(
-                    "Counter : jkcdjkscd ",
-                    // maxLines: 1,
-                    style: TextStyle(
-                      // color: Color(0x91DB7D7D),
-                      fontSize: 30,
-                      fontWeight: FontWeight.w100,
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    print("Clicked");
-                  },
-                  child: Text("Click"),
-                ),
-                IconButton(
-                  icon: Icon(Icons.accessibility),
-                  onPressed: () {},
-                )
-              ],
+              ),
             ),
-          ),
+            GestureDetector(
+              onTap: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context) => MovieDetails("Star wars IV"),
+                //   ),
+                // );
+              },
+              child: Container(
+                height: 120,
+                margin: EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Image.network(
+                          "https://m.media-amazon.com/images/I/A1wnJQFI82L._AC_SY879_.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Star wars IV",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 19,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Row(
+                              children: [
+                                Text(
+                                  "5 Starts",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Spacer(),
+                                Text(
+                                  "2.6k reviews",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                              ],
+                            ),
+                            SizedBox(height: 5),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.access_time,
+                                  size: 14,
+                                  color: Colors.grey[350],
+                                ),
+                                SizedBox(width: 4),
+                                Text(
+                                  "2h 14m",
+                                  style: TextStyle(
+                                    color: Colors.grey[350],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 5),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.access_time,
+                                  size: 14,
+                                  color: Colors.grey[350],
+                                ),
+                                SizedBox(width: 4),
+                                Text(
+                                  "2019/12/31",
+                                  style: TextStyle(
+                                    color: Colors.grey[350],
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text("Click"),
+    );
+  }
+}
+
+class MovieDetails extends StatelessWidget {
+  String? movieName;
+
+  MovieDetails({this.movieName});
+
+  @override
+  Widget build(BuildContext context) {
+    movieName = ModalRoute.of(context)?.settings.arguments as String?;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(movieName ?? ""),
+      ),
+      body: Container(
+        child: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Text("Back to Home screen"),
+        ),
       ),
     );
   }
